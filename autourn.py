b38abe3eb5191f8280b98782fbbab1f74de01bae
capazme <guglielmo.puzio00@gmail.com>
2024-03-27 22:05:34 +0100

    aggiunte istruzioni per GPT

--- autourn.py
+++ autourn.py
@@ -1,7 +1,6 @@
 import datetime
 import re
 from bs4 import BeautifulSoup
-from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
